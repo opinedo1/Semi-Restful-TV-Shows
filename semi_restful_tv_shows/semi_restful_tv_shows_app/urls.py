91c6cb01e3bef152d51b91e2df00d708d60fff64
Oscar Pinedo <opinedo19@gmail.com>
2021-05-24 03:39:56 -0700

    update edit and create implemented

--- semi_restful_tv_shows/semi_restful_tv_shows_app/urls.py
+++ semi_restful_tv_shows/semi_restful_tv_shows_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index),
     path('shows', views.shows),
     path('shows/<int:show_id>/edit', views.edit),
-    path('shows/<int:show_id>', views.view)
+    path('shows/<int:show_id>', views.view),
+    path('shows/create', views.new),
+    path('shows/<int:show_id>/update', views.update)
 ]
--- semi_restful_tv_shows/semi_restful_tv_shows_app/views.py
+++ semi_restful_tv_shows/semi_restful_tv_shows_app/views.py
@@ -22,3 +22,28 @@ def view(request, show_id):
         'show': Show.objects.get(id=show_id)
     }
     return render(request, 'view.html', context)
+
+def new(request):
+    if request.method == 'GET':
+        return render(request, 'new.html')
+    elif request.method == 'POST':
+        Show.objects.create(
+            title = request.POST['title'],
+            network = request.POST['network'],
+            release_date = request.POST['release_date'],
+            description = request.POST['description']  
+        )
+        show_id = Show.objects.last().id
+        return redirect("/shows/{}".format(show_id))
+
+def update(request, show_id):
+    if request.method == 'GET':
+        return redirect('/shows')
+    this_show = Show.objects.get(id=show_id)
+    this_show.title = request.POST['title']
+    this_show.release_date = request.POST['release_date']
+    this_show.network = request.POST['network']
+    this_show.description = request.POST['description']
+    this_show.save()
+    return redirect('/shows')
+    
