d836679b15ff79403af8489822b9abd900e45e4e
Oscar Pinedo <opinedo19@gmail.com>
2021-05-24 03:57:24 -0700

    added endpoint for delete impementation

--- semi_restful_tv_shows/semi_restful_tv_shows_app/urls.py
+++ semi_restful_tv_shows/semi_restful_tv_shows_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('shows/<int:show_id>/edit', views.edit),
     path('shows/<int:show_id>', views.view),
     path('shows/create', views.new),
-    path('shows/<int:show_id>/update', views.update)
+    path('shows/<int:show_id>/update', views.update),
+    path('shows/<int:show_id>/delete', views.delete)
 ]
