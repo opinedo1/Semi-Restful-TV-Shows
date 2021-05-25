f1a43cea9db78c307d635f31dccf02b705619d7f
Oscar Pinedo <opinedo19@gmail.com>
2021-05-24 21:56:08 -0700

    added validation for creating and updating forms

--- semi_restful_tv_shows/semi_restful_tv_shows_app/models.py
+++ semi_restful_tv_shows/semi_restful_tv_shows_app/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from datetime import datetime
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['title']) < 2:
+            errors["title"] = "Show title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Show network should be at least 3 characters"
+        if postData['description'] != '' and len(postData['description']) < 10:
+            errors["description"] = "Show description should be at least 10 characters"
+        if postData['release_date'] == '':
+            errors['release_date'] = 'Release Date field should be not be empty'
+        else:
+            if datetime.strptime(postData['release_date'], '%Y-%m-%d') > datetime.now():
+                errors['release_date'] = 'Release Date should be in the past'
+        return errors
+
 
 # Create your models here.
 class Show(models.Model):
@@ -8,3 +27,4 @@ class Show(models.Model):
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
