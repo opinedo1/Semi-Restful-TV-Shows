1b97381ae523979c866d64ff52519b55df52ebc0
Oscar Pinedo <opinedo19@gmail.com>
2021-05-24 22:00:43 -0700

    validation implemented in views and displayd on UI upon invalid form submissions

--- semi_restful_tv_shows/semi_restful_tv_shows_app/views.py
+++ semi_restful_tv_shows/semi_restful_tv_shows_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Show
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -11,6 +12,7 @@ def shows(request):
     }
     return render(request, 'shows.html', context)
 
+# Adding form validation to edit page
 def edit(request, show_id):
     context = {
         'show': Show.objects.get(id=show_id)
@@ -23,29 +25,44 @@ def view(request, show_id):
     }
     return render(request, 'view.html', context)
 
+# Adding form validation to new page
 def new(request):
     if request.method == 'GET':
         return render(request, 'new.html')
     elif request.method == 'POST':
-        Show.objects.create(
-            title = request.POST['title'],
-            network = request.POST['network'],
-            release_date = request.POST['release_date'],
-            description = request.POST['description']  
-        )
-        show_id = Show.objects.last().id
-        return redirect("/shows/{}".format(show_id))
+        errors = Show.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for (key, value) in errors.items():
+                messages.error(request, value)
+                print(errors)
+            return redirect('/shows/create')
+        else:
+            Show.objects.create(
+                title = request.POST['title'],
+                network = request.POST['network'],
+                release_date = request.POST['release_date'],
+                description = request.POST['description']  
+            )
+            show_id = Show.objects.last().id
+            return redirect("/shows/{}".format(show_id))
 
 def update(request, show_id):
     if request.method == 'GET':
         return redirect('/shows')
-    this_show = Show.objects.get(id=show_id)
-    this_show.title = request.POST['title']
-    this_show.release_date = request.POST['release_date']
-    this_show.network = request.POST['network']
-    this_show.description = request.POST['description']
-    this_show.save()
-    return redirect('/shows')
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for (key, value) in errors.items():
+                messages.error(request, value)
+                print(errors)
+        return redirect('/shows/{}/edit'.format(show_id))
+    else:
+        this_show = Show.objects.get(id=show_id)
+        this_show.title = request.POST['title']
+        this_show.release_date = request.POST['release_date']
+        this_show.network = request.POST['network']
+        this_show.description = request.POST['description']
+        this_show.save()
+        return redirect('/shows')
     
 def delete(request, show_id):
     if request.method == 'GET':
